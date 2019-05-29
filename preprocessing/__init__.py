471ecf5b12587841552e6025f26966478d145fc5
Oscar <opizarro73@gmail.com>
2019-05-29 13:28:37 +0200

    add preprocessors

---
+++ preprocessing/__init__.py
@@ -0,0 +1,4 @@
+# import Classes into the package level so they can be imported from this package
+from .croprotflippreprocessor import CropRotFlipPreprocessor
+from .imagetoarraypreprocessor import ImageToArrayPreprocessor
+from .patchpreprocessor import PatchPreprocessor
